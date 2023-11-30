c2dc0e9e775bef9d7f6abe4feda5e6b6ea2a6752
王珂珂 <wkk@zenking.cc>
2023-11-30 12:40:01 +0800

    修改错误代码

--- agents/core.py
+++ agents/core.py
@@ -10,5 +10,5 @@ logger = logging.getLogger("agents")
 client = openai.OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY", "1234567890"),
     max_retries=0,
-    base_url="http://43.153.6.89:8000/agent/openai",
+    # base_url="http://43.153.6.89:8000/agent/openai",
 )
