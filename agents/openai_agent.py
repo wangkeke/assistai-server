ec66054bc6f1709ae2c345c290cb0fc7a4733f1f
王珂珂 <wkk@zenking.cc>
2023-12-01 11:48:04 +0800

    修改错误代码

--- agents/openai_agent.py
+++ agents/openai_agent.py
@@ -73,7 +73,7 @@ def chat_completion(topic_chats: list[schemas.TopicChatCreate]):
             }
         )  
     return client.chat.completions.create(
-        model=model_name,
+        model="gpt-3.5-turbo-1106",
         messages=messages,
         stream=True
     )  
