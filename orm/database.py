2ca6aa6a97a2bb7a4fdd27b2a43f90ca389a88df
王珂珂 <wkk@zenking.cc>
2023-12-04 09:36:43 +0800

    修改错误代码

--- orm/database.py
+++ orm/database.py
@@ -4,6 +4,6 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 DATASOURCE_URL = os.getenv('DATASOURCE_URL', 'mysql+mysqldb://root:12345678@127.0.0.1:3306/uassistant?charset=utf8mb4&')
 # DATASOURCE_URL = os.getenv('DATASOURCE_URL', 'postgresql://root:LQSrJuf3nSdEchzulbMrkVOyIsuDg0fx@dpg-ck55avug2bec73cgb9og-a.oregon-postgres.render.com/assistai')
-engine = create_engine(url=DATASOURCE_URL)
+engine = create_engine(url=DATASOURCE_URL, pool_recycle=3600, pool_pre_ping=True, pool_size=10)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
