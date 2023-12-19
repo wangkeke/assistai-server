b01230d0bfecb77954f25ee58ba2ac07aa9c0467
王珂珂 <wkk@zenking.cc>
2023-12-19 15:23:49 +0800

    修改错误代码

--- agents/util.py
+++ agents/util.py
@@ -13,6 +13,7 @@ def encode_image(image_url: str):
     return base64.b64encode(image_file.read()).decode('utf-8')
 
 def abatch_tasks(tool_functions: list):
+  nest_asyncio.apply()
   return asyncio.run(execute(tool_functions))
 
 def execute(tool_functions: list):
