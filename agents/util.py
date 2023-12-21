56cc365bc00d939d796e401537935fd878172c76
王珂珂 <wkk@zenking.cc>
2023-12-21 10:17:41 +0800

    修改错误代码

--- agents/util.py
+++ agents/util.py
@@ -19,11 +19,11 @@ def execute(tool_functions: list):
 
 def max_encoding_tokens(encoding_name: str = "gpt-3.5-turbo") -> int:
   """Returns the max of tokens for a encoding."""
-  encoding = tiktoken.get_encoding(encoding_name)
+  encoding = tiktoken.encoding_for_model(encoding_name)
   return encoding.max_token_value
 
 def num_tokens_from_string(string: str, encoding_name: str = "gpt-3.5-turbo") -> int:
   """Returns the number of tokens in a text string."""
-  encoding = tiktoken.get_encoding(encoding_name)
+  encoding = tiktoken.encoding_for_model(encoding_name)
   num_tokens = len(encoding.encode(string))
   return num_tokens
